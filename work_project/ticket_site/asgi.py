f0ca0965b4168ac6a607dc8ec3620f9a9360ebfa
Horoiu Cezar <horoiucezar@yahoo.comgit config --global user.name Horoiu Cezargit statusrungit ipgit config --global user.email horoiucezar@yahoo.com>
2023-05-08 20:12:33 +0300

    changes in project structure

--- work_project/ticket_site/asgi.py
+++ work_project/ticket_site/asgi.py
@@ -1,5 +1,9 @@
 """
+<<<<<<<< HEAD:work_project/ticket_site/asgi.py
 ASGI config for ticket_site project.
+========
+ASGI config for work_project project.
+>>>>>>>> 9275fdb (change name project):work_project/work_project/asgi.py
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +15,10 @@ import os
 
 from django.core.asgi import get_asgi_application
 
+<<<<<<<< HEAD:work_project/ticket_site/asgi.py
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ticket_site.settings')
+========
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'work_project.settings')
+>>>>>>>> 9275fdb (change name project):work_project/work_project/asgi.py
 
 application = get_asgi_application()
