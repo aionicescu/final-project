bf98450eaf61590415b5632f9617626c6af1c4a5
Horoiu Cezar <horoiucezar@yahoo.comgit config --global user.name Horoiu Cezargit statusrungit ipgit config --global user.email horoiucezar@yahoo.com>
2023-05-09 20:51:09 +0300

    Change Cezar

--- work_project/ticket_site/models.py
+++ work_project/ticket_site/models.py
@@ -1,15 +1,19 @@
 from django.db import models
 
+from event.models import Event, TicketStatus
+
 
 class Ticket(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     email = models.EmailField(max_length=30)
     description = models.TextField(max_length=300)
-    active = models.BooleanField(default=True)
-    start_date = models.DateField()
-    end_date = models.DateField()
-    # event = models.ForeignKey(Event, on_delete=models.CASCADE, null=True)
+    status = models.CharField(max_length=100, choices=TicketStatus)
+
+    # active = models.BooleanField(default=True)
+    # start_date = models.DateField()
+    # end_date = models.DateField()
+    event = models.ForeignKey(Event, on_delete=models.CASCADE, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
--- work_project/ticket_site/settings.py
+++ work_project/ticket_site/settings.py
@@ -39,8 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'ticket_site',
-    'home'
 ]
 
 MIDDLEWARE = [
