c95b3ef5b7b170567c8c811ed10ccf775ae5dd01
Horoiu Cezar <horoiucezar@yahoo.comgit config --global user.name Horoiu Cezargit statusrungit ipgit config --global user.email horoiucezar@yahoo.com>
2023-05-10 16:06:36 +0300

    user models

--- work_project/user/models.py
+++ work_project/user/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+from event.models import Event
+
+
+class User(models.Model):
+    gender_options = (('male', 'Male'), ('female', 'Female'))
+
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    age = models.IntegerField()
+    email = models.EmailField(max_length=30)
+    description = models.TextField(max_length=300)
+    active = models.BooleanField(default=True)
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
+    gender = models.CharField(max_length=6, choices=gender_options)
+    event = models.ForeignKey(Event, on_delete=models.CASCADE, null=True)
+
+    created_at = models.DateTimeField(auto_now_add=True,
+                                      null=True)
+    updated_at = models.DateTimeField(auto_now=True,
+                                      null=True)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
