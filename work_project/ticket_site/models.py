4a2ebd353006a9452afe44c2a2b31ab32a882f6a
aionicescu <ionicescu.alexandrugabriel@gmail.com>
2023-05-16 18:53:55 +0300

    Class Order from ticket_site

--- work_project/ticket_site/models.py
+++ work_project/ticket_site/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 from event.models import CHOICES,  Eveniment
+from user.models import User
 
 
 class Ticket(models.Model):
@@ -19,3 +20,15 @@ class Ticket(models.Model):
 
     def __str__(self):
         return f'{self.first_name} {self.last_name}'
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    tickets = models.ManyToManyField(Ticket)
+    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+    date_ordered = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Order by {self.user.username} on {self.date_ordered}"
+
+    class Meta:
+        verbose_name_plural = "Orders"
