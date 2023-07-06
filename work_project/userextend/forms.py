279e9c83a8a8d2bd7032cb8b61e4b99369dafe22
Horoiu Cezar <horoiucezar@yahoo.comgit config --global user.name Horoiu Cezargit statusrungit ipgit config --global user.email horoiucezar@yahoo.com>
2023-07-06 11:26:05 +0300

    Revert "rezolvare bug"
    This reverts commit abe732b8

--- work_project/userextend/forms.py
+++ work_project/userextend/forms.py
@@ -7,14 +7,14 @@ class UserForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['first_name', 'last_name', 'email',
-                  'username'
+                  # 'username'
                   ]
 
         widgets = {
             'first_name': TextInput(attrs={'class': 'form-control', 'placeholder': 'Please enter your first name'}),
             'last_name': TextInput(attrs={'class': 'form-control', 'placeholder': 'Please enter your last name'}),
             'email': EmailInput(attrs={'class': 'form-control', 'placeholder': 'Please enter your email'}),
-            'username': TextInput(attrs={'class': 'form-control', 'placeholder': 'Please enter your username'}),
+            # 'username': TextInput(attrs={'class': 'form-control', 'placeholder': 'Please enter your username'}),
         }
 
     def __init__(self, *args, **kwargs):
